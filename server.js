e34759aa6ce1a64d513875fd432a3d08cec0ed67
David Magueijo <davedev1984@gmail.com>
2020-11-12 15:02:07 +0000

    Crossed fingers

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send('it is working!') })
 
-app.post('/signin', signin.handleSignin(db, bcrypt)(req, res))
+app.post('/signin', signin.handleSignin(db, bcrypt))
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
